f4ef379ab6f5003836eebff2067a2514244fb5da
Apoorva1217 <apoorvarasal12@gmail.com>
2020-11-20 17:05:45 +0530

    [Apoorva] Add . Ability to create a employee payroll table in the payroll service database to manage employee payrolls

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -1,3 +1,11 @@
 --UC1 Ability to create a payroll service database
 CREATE DATABASE Payroll_Service;
 USE Payroll_Service;
+
+--UC2 Ability to create a employee payroll table in the payroll service database to manage employee payrolls
+CREATE TABLE Employee_Payroll(
+EmpId INT PRIMARY KEY IDENTITY(1,1),
+EmpName VARCHAR(20) NOT NULL,
+Salary MONEY NOT NULL,
+Start_Date DATE NOT NULL 
+);
